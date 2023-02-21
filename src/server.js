c440c5e7a1e0a8f2c3fbe2f865ce8ccd10722040
백은주 <specialguest@kakao.com>
2023-02-21 21:31:56 +0900

    feat: CORS 설정

--- src/server.js
+++ src/server.js
@@ -9,10 +9,18 @@ const fruitsRouter = require('./routes/fruits');
 const app = express();
 const server = http.createServer(app);
 
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', '*');
+
+  next();
+});
+
 app.use('/fruits', fruitsRouter);
 
 const host = 'localhost';
-const port = 3000;
+const port = 8000;
 
 server.listen(port, host, () => {
   console.info(`Server address: ${host}:${port}`);
